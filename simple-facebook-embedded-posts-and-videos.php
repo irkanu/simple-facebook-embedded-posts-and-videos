9013662fe3de9a0ec35def563b415f8ac88c4c85
dylan ryan <drryan@ncsu.edu>
2015-05-15 17:19:11 -0400

    Initial commit

---
+++ simple-facebook-embedded-posts-and-videos.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name:    Simple Facebook Embedded Posts and Videos
+ * Plugin URI:     todo
+ * Description:    Easily embed Facebook posts and videos.
+ * Version:        1.0.0
+ * Author:         Dylan Ryan
+ * Author URI:     https://profiles.wordpress.org/irkanu
+ * Domain Path:    /languages
+ * Text Domain:    simple-facebook-embedded
+ * GitHub URI:     https://github.com/irkanu/simple-facebook-embedded
+ * GitHub Branch:  master
+ * License:        GPL v3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package     Simple_Facebook
+ * @subpackage  Simple_Facebook_Page_Plugin
+ * @author      Dylan Ryan
+ * @version     1.4.4
+ */
